03f15f71636f87d08269cf20699320e07233e9ca
rishabhbhatt009 <rishabhbhatt009@gmail.com>
2023-06-12 15:50:09 -0400

    understanding admin

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -13,6 +13,11 @@ class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey(to='Product', on_delete=models.SET_NULL, null=True, related_name='+')
     
+    def __str__(self) -> str:
+        return self.title
+    
+    # class Meta : 
+    #     ordering = ['title']
 
 class Product(models.Model):    
 
@@ -28,6 +33,11 @@ class Product(models.Model):
     collection = models.ForeignKey(to=Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(to=Promotion)
     
+    def __str__(self) -> str:
+        return self.title
+    
+    # class Meta : 
+    #     ordering = ['title']
 
 class Customer(models.Model):
     
