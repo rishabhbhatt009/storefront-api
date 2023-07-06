8d7838ae1642a2fa07233959e6302ad68e493b8e
rishabhbhatt009 <rishabhbhatt009@gmail.com>
2023-07-06 18:39:26 -0400

    WIP - cart app

--- storefront2/store/models.py
+++ storefront2/store/models.py
@@ -1,5 +1,6 @@
 from django.core.validators import MinValueValidator
 from django.db import models
+from uuid import uuid4
 
 ##########################################################################################
 # Models used from course - 1 
@@ -96,21 +97,29 @@ class Address(models.Model):
     customer = models.ForeignKey(
         Customer, on_delete=models.CASCADE)
 
+##########################################################################################
+# Cart Model Implementation 
+##########################################################################################
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
+    
+    # cart and product should be unique 
+    class Meta :
+        unique_together = [['cart', 'product']]
+    
+
 
 ##########################################################################################
-# New Models 
-##########################################################################################
-
 # Product Review Model 
+##########################################################################################
 
 class Review(models.Model):
     # fields 
