4d3c61ccee39b4f9d1a5bb7c56e2518c392908f5
rishabhbhatt009 <rishabhbhatt009@gmail.com>
2023-07-28 18:05:20 -0400

    created custom user models

--- storefront2/store/admin.py
+++ storefront2/store/admin.py
@@ -78,12 +78,15 @@ class CollectionAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
+    ### updating field references by adding functions for field
     list_display = ['first_name', 'last_name',  'membership', 'orders']
     list_editable = ['membership']
     list_per_page = 10
-    ordering = ['first_name', 'last_name']
+    list_select_related = ['user']
+    ### updating fields references, which now come from user model 
+    ordering = ['user__first_name', 'user__last_name']
     search_fields = ['first_name__istartswith', 'last_name__istartswith']
-
+    
     @admin.display(ordering='orders_count')
     def orders(self, customer):
         url = (
